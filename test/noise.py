54f5db5219b674027d3c42e5b8ed73b52f446356
Gabriel Cruz <gabs.oficial98@gmail.com>
2019-05-28 16:37:02 -0300

    First working version of fft

--- test/noise.py
+++ test/noise.py
@@ -1,14 +1,23 @@
 import numpy as np
-from scipy.fftpack import fft2, ifft2
+from scipy.fftpack import fft2, ifft2, fftshift
 import imageio
 import matplotlib.pyplot as plt
 from skimage.util import random_noise
+from matplotlib.colors import LogNorm
 
 
-img = imageio.imread('images/arara.png')
+
+
+def rgb2gray(rgb):
+    return np.dot(rgb[...,:3], [0.2989, 0.5870, 0.1140])
+
+
+
+img = imageio.imread('images/cat_original.jpg')
+img = rgb2gray(img)
 img_fft = fft2(img)
-plt.imshow(np.abs(img_fft), cmap='gray')
-# plt.imshow(np.abs(img_fft), cmap='gray')
+img_fft_shift = fftshift(img_fft)
+plt.imshow(np.abs(img_fft_shift), cmap='gray', norm=LogNorm(vmin=5))
 plt.show()
 
 """
