Fix low-pass, change silent accordingly

silent.py
@@ -26,12 +26,11 @@ def noise_menu():
 
 
 def filter_menu():
-    print(" Choose four filter options (e.g. 2 1 3 0)")
+    print(" Choose three filter options (e.g. 2 1 0)")
     print("(0) No filter")
     print("(1) Median filter")
     print("(2) Cut filter")
     print("(3) Low pass filter")
-    print("(4) High pass filter")
 
     options = [int(opt) for opt in input().split()]
 
@@ -40,7 +39,7 @@ def filter_menu():
 
 if __name__ == '__main__':
 
-    if sys.argc != 2:
+    if len(sys.argv) != 2:
         print('usage: python silent.py <INPUT IMAGE>')
         exit(0)
 
@@ -65,50 +64,29 @@ if __name__ == '__main__':
         3: src.noises.diagonal,
     }
 
-    img_gray_noisy = np.copy(img_gray)
-
-    for noise_opt in noise_options:
-        img_gray_noisy = noises[noise_opt](img_gray_noisy, 3)
-        plt.imshow(img_gray_noisy, cmap='gray')
-        plt.show()
-
     # Choose and apply filter functions
     filter_options = filter_menu()
+
     filters = {
         1: src.filters.median,
         2: src.filters.cut,
         3: src.filters.low_pass,
-        4: src.filters.high_pass,
     }
 
     # Apply chosen noises
+    img_gray_noisy = np.copy(img_gray)
+    for noise_opt in noise_options:
+        if noise_opt != 0:
+            img_gray_noisy = noises[noise_opt](img_gray_noisy, 3)
+            plt.imshow(img_gray_noisy, cmap='gray')
+            plt.show()
+
+    # Apply chosen filters
     img_gray_filtered = np.copy(img_gray_noisy)
-
     for filter_opt in filter_options:
-        img_gray_filtered = filters[filter_opt](img_gray_filtered)
-        plt.imshow(img_gray_filtered, cmap='gray')
-        plt.show()
+        if filter_opt != 0:
+            img_gray_filtered = filters[filter_opt](img_gray_filtered)
+            plt.imshow(img_gray_filtered, cmap='gray')
+            plt.show()
 
-
-    # TODO: RELOCATE THIS PART OF THE CODE
-    if(option == 1):
-        img_filtered = median_filter(img)       #aply median filter
-
-    elif(option == 2):
-
-    elif(option == 3):
-        img_fft = fftn(img)                             #aply fourier transform
-        img_fft_shift = fftshift(img_fft)               #aply shift into the fourier image
-        plt.imshow(np.abs(img_fft_shift), cmap='gray', norm=LogNorm(vmin=5))
-        plt.show()                                      #show the spectrum
-        filt = low_pass(501, img.shape)                 #create low pass filter
-        img_fft_shift_filtered = img_fft_shift * filt   #aply the filter in the fourier image
-        plt.imshow(np.abs(img_fft_shift_filtered), cmap='gray', norm=LogNorm(vmin=5))
-        plt.show()                                      #show low filter pass spectrum 
-        res = ifftn( fftshift(img_fft_shift_filtered) ) # create result image using inverse
-        plt.imshow(np.abs(res), cmap='gray', norm=LogNorm(vmin=5))
-        plt.show()                                      #show the result image
-
-    else:
-        print("valor inválido")
 

src/filters.py
@@ -1,19 +1,17 @@
 import numpy as np
-from scipy.fftpack import *
+from scipy.fftpack import fftn, ifftn, fftshift
+from scipy.spatial.distance import euclidean
 import imageio
 import matplotlib.pyplot as plt
+from matplotlib.colors import LogNorm
 
 
-# TODO: implement image receival and fourier transformation
-# OBS: no longer returns filter, but filtered image
-def low_pass(img, radius=50, debug=False):
-    """
-    Filters image using low pass filter in Fourier domain
-    Returns filtered image
-    """
+def _low_pass_filter(radius, final_shape):
+    filt = np.zeros((radius, radius))
 
-    for x in range(radius):     #create a circular area with values 1
-        for y in range(radius): # this will be the pass region
+    # Create circle mask (pad later)
+    for x in range(radius):
+        for y in range(radius):
             if (radius//2 - x)**2 + (radius//2 - y)**2 < (radius//2)**2:
                 filt[x][y] = 1
 
@@ -21,70 +19,74 @@ def low_pass(img, radius=50, debug=False):
     plt.imshow(filt, cmap='gray')
     plt.show()
 
-    # calculate padding shape
-    # TODO: fix even/odd cases
-    aux1 = final_shape[0] - filt.shape[0]   #calculate the difference betwen
-    if(aux1%2 == 0):                         #the circular filter and the real image
+    # Calculate padding shape (rows)
+    aux1 = final_shape[0] - filt.shape[0]
+    if(aux1 % 2 == 0): # even vs odd problem
         pad_cols = ( (final_shape[0] - filt.shape[0])//2, \
                     (final_shape[0] - filt.shape[0])//2)
-    else:                                   #verify size based on the difference
+    else:
         pad_rows = ( (final_shape[0] - filt.shape[0])//2+1, \
                      (final_shape[0] - filt.shape[0])//2+1 )
 
-    aux2 = final_shape[1] - filt.shape[1]   #calculate the difference betwen
-    if(aux2%2 == 0):                        #the circular filter and the real image
+    # Calculate padding shape (cols)
+    aux2 = final_shape[1] - filt.shape[1]
+    if(aux2 % 2 == 0):
         pad_cols = ( (final_shape[1] - filt.shape[1])//2, \
                      (final_shape[1] - filt.shape[1])//2)
-    else:                                   #verify size based on the difference
+    else:
         pad_cols = ( (final_shape[1] - filt.shape[1])//2+1, \
                      (final_shape[1] - filt.shape[1])//2+1)
 
     pad_shape = (pad_rows, pad_cols)
 
-    # padding
+    # Apply padding
     filt = np.pad(filt, pad_shape, 'constant', constant_values=0)
-                                             #check for even numbers
-    if(aux1%2 != 0 and aux2%2 != 0):         #if needed remove row and/or coluns
+    if(aux1 % 2 != 0 and aux2 % 2 != 0):
         filt = filt[0:filt.shape[0]-1, 0:filt.shape[1]-1]
-    elif(aux1%2 != 0 and aux2%2 == 0):
+    elif(aux1 % 2 != 0 and aux2 % 2 == 0):
         filt = filt[0:filt.shape[0]-1, 0:filt.shape[1]]
-    elif(aux1%2 == 0 and aux2%2 != 0):
+    elif(aux1 % 2 == 0 and aux2 % 2 != 0):
         filt = filt[0:filt.shape[0], 0:filt.shape[1]-1]
 
     print(filt.shape)
-    plt.imshow(filt, cmap='gray')
+    plt.imshow(filt, cmpa)
     plt.show()
 
-    if debug:
-        plt.imshow(filt)
-        plt.show()
-
     return filt
 
 
-# TODO: implement (receive img return img)
-def high_pass(img, debug=False):
-    filt = np.ones((radius, radius))
+def low_pass(img, radius=501, debug=False):
+    """
+    Filters image using low pass filter in Fourier domain
+    Returns filtered image
+    """
 
-    for x in range(radius):
-        for y in range(radius):
-            if (radius//2 - x)**2 + (radius//2 - y)**2 < (radius//2)**2:
-                filt[x][y] = 0
+    img_fft = fftn(img)
+    img_fft_shift = fftshift(img_fft)
 
-    if debug:
-        plt.imshow(filt)
-        plt.show()
+    # Print spectrum before filtering
+    plt.imshow(np.abs(img_fft_shift), cmap='gray', norm=LogNorm(vmin=5))
+    plt.show()
 
-    return filt
+    filter_mask = _low_pass_filter(radius, img.shape)
+
+    img_fft_shift_filtered = img_fft_shift * filter_mask
+
+    # Print spectrum after filtering
+    plt.imshow(np.abs(img_fft_shift_filtered), cmap='gray', norm=LogNorm(vmin=5))
+    plt.show()
+
+    # Generate result image
+    return ifftn(fftshift(img_fft_shift_filtered))
 
 
-def _median(img, x, y, k):
+def _local_median(img, x, y, k):
     """
     Computes median for k-neighborhood of img[x,y]
     """
     flat = img[x-k : x+k+1, y-k : y+k+1].flatten()
     flat.sort()
-    res[x, y] = flat[len(flat)//2]
+    return flat[len(flat)//2]
 
 
 def median(img, k=3):
@@ -95,8 +97,9 @@ def median(img, k=3):
 
     for x in range(img.shape[0]):
         for y in range(img.shape[1]):
-            if (x-k >= 0 and x+k < m) and (y-k >= 0 and y+k < n):
-                res[x, y] = _median(x, y, k)
+            if (x-k >= 0 and x+k < img.shape[0]) and \
+                    (y-k >= 0 and y+k < img.shape[1]):
+                res[x, y] = _local_median(img, x, y, k)
 
     return res
 
@@ -126,8 +129,6 @@ def cut(img):
     plt.show()
 
     # Return to space domain result image using inverse
-    res = ifftn( fftshift(img_fft_shift_filtered) )
-
-    return res
+    return ifftn( fftshift(img_fft_shift_filtered) )
 
 